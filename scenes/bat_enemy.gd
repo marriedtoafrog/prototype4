b441738bd3b33a156e09c958d505db94c0240e1f
marriedtoafrog <michellezhou1024@gmail.com>
2024-03-29 17:52:03 -0400

    updating enemy ai

--- scenes/bat_enemy.gd
+++ scenes/bat_enemy.gd
@@ -1,28 +1,34 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var speed = 50
+var playerChase = false 
+var player = null 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if playerChase:
+		position += (player.position - position).normalized() * speed * delta
+		move_and_collide(Vector2(0,0)) 
+		if(player.position.x - position.x) < 0: 
+			#$AnimatedSprite2D.flip_h = true 
+			$AnimatedSprite2D.play("move left")
+			#$AnimatedSprite2D.flip_h = true #going left 
+		else: 
+			#$AnimatedSprite2D.flip_h = false 
+			$AnimatedSprite2D.play("move right")
+			
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = lerp(velocity, Vector2.ZERO, 0.05)
+		move_and_collide(velocity) 
+		$AnimatedSprite2D.play("move front") 
+		
+		
+		
+#enters the zone 
+func _on_detection_area_body_entered(body):
+	player = body 
+	playerChase = true 
 
-	move_and_slide()
+
+func _on_detection_area_body_exited(body):
+	player = null 
+	playerChase = false 
