5a194eef5c3ac3eaf0f3468003ebc8dd42f00d13
marriedtoafrog <michellezhou1024@gmail.com>
2024-03-31 01:08:59 -0400

    end scene and win

---
+++ scenes/end.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func _on_go_back_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/start menu.tscn")
--- scenes/game manager.gd
+++ scenes/game manager.gd
@@ -7,20 +7,28 @@ var blue_gems = 0
 var purple_gems = 0
 
 func add_blue(): 
-	blue_gems += 1
-	print(blue_gems)
+	if(purple_gems != 4 and blue_gems != 4):
+		blue_gems += 1
+		print(blue_gems)
 	#points_label.text = "Blue Gems: " + str(blue_gems) 
 	#points_label.text = "Purple Gems: " + str(purple_gems) 
-	blue_label.text = "Blue Gems: " + str(blue_gems)
-	purple_label.text = "Purple Gems: " + str(purple_gems)
+		blue_label.text = "Blue Gems: " + str(blue_gems)
+		purple_label.text = "Purple Gems: " + str(purple_gems)
+	else: 
+		get_tree().change_scene_to_file("res://scenes/end.tscn")
 	
 func add_purple(): 
-	purple_gems += 1
-	print(purple_gems)
+	if(purple_gems != 4 and blue_gems != 4):
+		purple_gems += 1
+		print(purple_gems)
 	#points_label.text = "Purple Gems: " + str(purple_gems) 
 	#points_label.text = "Purple Gems: " + str(blue_gems) 
-	blue_label.text = "Blue Gems: " + str(blue_gems)
-	purple_label.text = "Purple Gems: " + str(purple_gems)
+		blue_label.text = "Blue Gems: " + str(blue_gems)
+		purple_label.text = "Purple Gems: " + str(purple_gems)
+	else: 
+		get_tree().change_scene_to_file("res://scenes/end.tscn")
+	
+
 	
 	
 
