3a32fa50c978e8f724139e37048ced2f71262578
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2024-03-31 11:30:35 -0400

    ranged pathfinding

--- scenes/bat_enemy.gd
+++ scenes/bat_enemy.gd
@@ -5,17 +5,21 @@ const speed = 50
 @export var player: Node2D
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
 
+# follow only if the player is in range
+@export var trigger_distance: float
 
 func _physics_process(_delta:float) -> void:
 	
 	var dir = to_local(nav_agent.get_next_path_position()).normalized()
 	velocity = dir * speed
-	move_and_slide()
+	
+	if (nav_agent.distance_to_target() <= trigger_distance):
+		move_and_slide()
 	
 
 func make_path() -> void:
-	print("making path")
 	nav_agent.target_position = player.global_position
 
 func _on_timer_timeout():
+	# make a path every _ seconds
 	make_path() 
