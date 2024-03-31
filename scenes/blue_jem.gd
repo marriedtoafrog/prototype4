45a8e9ccbc9982ae5ea7a9581caf8758da1d8c63
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2024-03-31 18:15:18 -0400

    rename sounds

--- scenes/blue_jem.gd
+++ scenes/blue_jem.gd
@@ -6,7 +6,7 @@ var is_collected = false
 func _on_body_entered(body):
 	if(body.name == "main character") and not is_collected:
 		is_collected = true
-		$"../AudioStreamPlayer".play()
+		$"../GemNoise".play()
 		#queue_free()
 		hide()
 		queue_free()
