8335e29eef6e2a16ce353a01d576c29a8ed4b752
Salia Nahshal <snn2126@barnard.edu>
2024-03-31 12:41:01 -0400

    added sounds

--- scenes/blue_jem.gd
+++ scenes/blue_jem.gd
@@ -2,7 +2,12 @@ extends Area2D
 
 @onready var game_manager = %"game manager"
 
+var is_collected = false
+
 func _on_body_entered(body):
-	if(body.name == "main character"):
-		queue_free()
-		game_manager.add_blue()
+	if(body.name == "main character") and not is_collected:
+		is_collected = true
+		$"../AudioStreamPlayer".play()
+		#queue_free()
+		hide()
+		game_manager.add_purple()
